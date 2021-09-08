1f11b6743b199bd4419cf6942ed4879b74476337
Haruki Hirai <iamprivateengineer@gmail.com>
2021-09-08 17:58:55 +0900

    [add] number-string.ts

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1996;
+// year = false;
+console.log(year);
+
+let age: number = 25;
+
+console.log(age);
+
+let name: string = 'Haruki';
+// name = 10;
+console.log(name)
